b84d24ace0bc86f4e44051a14f8ac7fe66063887
Aðalsteinn Rúnarsson <adalsteinn.runarsson@netapp.com>
2020-02-06 11:08:07 +0000

    first commit

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/allir/go-prom
+
+require (
+	github.com/gorilla/mux v1.7.3
+	github.com/prometheus/client_golang v1.4.0
+)
+
+go 1.13
---
+++ main.go
@@ -0,0 +1,96 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+
+	"github.com/prometheus/client_golang/prometheus"
+    //"github.com/prometheus/client_golang/prometheus/promauto"
+    "github.com/prometheus/client_golang/prometheus/promhttp"
+)
+
+
+var (
+	inFlightGauge = prometheus.NewGauge(prometheus.GaugeOpts{
+		Name: "hello_requests_in_flight",
+		Help: "A gauge of requests currently being served by the wrapped handler.",
+	})
+
+	counter = prometheus.NewCounterVec(
+		prometheus.CounterOpts{
+			Name: "hello_requests_total",
+			Help: "A counter for requests to the wrapped handler.",
+		},
+		[]string{"code", "method"},
+	)
+
+	requestDuration = prometheus.NewHistogramVec(
+		prometheus.HistogramOpts{
+        	Name:    "hello_request_duration_seconds",
+        	Help:    "Time (in seconds) spent serving HTTP requests.",
+        	Buckets: prometheus.DefBuckets,
+		},
+		[]]string{"handler", "method"})
+	
+	responseSize = prometheus.NewHistogramVec(
+		prometheus.HistogramOpts{
+			Name:    "hello_response_size_bytes",
+			Help:    "A histogram of response sizes for requests.",
+			Buckets: []float64{200, 500, 900, 1500},
+		},
+		[]string{},
+	)
+
+	helloChain = promhttp.InstrumentHandlerInFlight(inFlightGauge,
+		promhttp.InstrumentHandlerDuration(requestDuration.MustCurryWith(prometheus.Labels{"handler": "hello"}),
+		promhttp.InstrumentHandlerCounter(counter,
+			promhttp.InstrumentHandlerResponseSize(responseSize, SayHello()),
+			),
+		),
+	)
+)
+
+func init() {
+	// Metrics have to be registered to be exposed:
+	prometheus.MustRegister(
+		inFlightGauge,
+		counter,
+		requestDuration,
+		responseSize,
+	)
+}
+
+func main() {
+	log.Println("Starting server, listening on port 8080")
+	router := mux.NewRouter()
+	router.Handle("/hello", helloChain)
+	router.Handle("/hello/{name}", helloChain)
+	router.Handle("/metrics", promhttp.Handler())
+	log.Fatal(http.ListenAndServe(":8080", router))
+}
+
+func SayHello() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		code := http.StatusBadRequest // if req is not GET
+		if r.Method == "GET" {
+			code = http.StatusOK
+			vars := mux.Vars(r)
+			name := vars["name"]
+
+			if name == "" {
+			    log.Println(r.URL.Path + " - 200 - Saying hello")
+			} else {
+				log.Println(r.URL.Path + " - 200 - Saying hello to " + name)
+			}
+
+			greet := fmt.Sprintf("Hello %s \n", name)
+			
+			w.Write([]byte(greet))
+		} else {
+			w.WriteHeader(code)
+		}
+	}
+}
