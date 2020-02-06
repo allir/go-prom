fe024dde8642c3d11aa9aa58dc1cb1cc00ab2361
Aðalsteinn Rúnarsson <adalsteinn.runarsson@netapp.com>
2020-02-06 13:09:37 +0000

    Add Bingo Handler

--- main.go
+++ main.go
@@ -34,7 +34,7 @@ var (
 			//Buckets: prometheus.DefBuckets,
 			Buckets: []float64{.25, .5, 1, 2.5, 5, 10},
 		},
-		[]string{},
+		[]string{"handler"},
 	)
 
 	responseSize = prometheus.NewHistogramVec(
@@ -48,9 +48,17 @@ var (
 	)
 
 	helloChain = promhttp.InstrumentHandlerInFlight(inFlightGauge,
-		promhttp.InstrumentHandlerDuration(requestDuration,
+		promhttp.InstrumentHandlerDuration(requestDuration.MustCurryWith(prometheus.Labels{"handler": "hello"}),
 		promhttp.InstrumentHandlerCounter(counter,
 			promhttp.InstrumentHandlerResponseSize(responseSize, SayHello()),
+			),
+		),
+	)
+
+	bingoChain = promhttp.InstrumentHandlerInFlight(inFlightGauge,
+		promhttp.InstrumentHandlerDuration(requestDuration.MustCurryWith(prometheus.Labels{"handler": "bingo"}),
+		promhttp.InstrumentHandlerCounter(counter,
+			promhttp.InstrumentHandlerResponseSize(responseSize, Bingo()),
 			),
 		),
 	)
@@ -71,10 +79,12 @@ func main() {
 	router := mux.NewRouter()
 	router.Handle("/hello", helloChain)
 	router.Handle("/hello/{name}", helloChain)
+	router.Handle("/bingo", bingoChain)
 	router.Handle("/metrics", promhttp.Handler())
 	log.Fatal(http.ListenAndServe(":8080", router))
 }
 
+// SayHello says hi
 func SayHello() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		code := http.StatusBadRequest // if req is not GET
@@ -84,12 +94,30 @@ func SayHello() http.HandlerFunc {
 			name := vars["name"]
 
 			if name == "" {
-			    log.Println(r.URL.Path + " - 200 - Saying hello")
+			    log.Println(r.URL.Path + " - GET - 200 - Saying hello")
 			} else {
-				log.Println(r.URL.Path + " - 200 - Saying hello to " + name)
+				log.Println(r.URL.Path + " - GET - 200 - Saying hello to " + name)
 			}
 
 			greet := fmt.Sprintf("Hello %s \n", name)
+			
+			w.Write([]byte(greet))
+		} else {
+			w.WriteHeader(code)
+		}
+	}
+}
+
+// Bingo says BINGO!
+func Bingo() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		code := http.StatusBadRequest // if req is not GET
+		if r.Method == "GET" {
+			code = http.StatusOK
+
+			log.Println(r.URL.Path + " - GET - 200 - BINGO")
+
+			greet := fmt.Sprintf("BINGO!\n")
 			
 			w.Write([]byte(greet))
 		} else {
