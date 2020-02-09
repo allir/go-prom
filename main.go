7d6a70f3d498a0dcbb0e5c633127373324cac391
Aðalsteinn Rúnarsson <adalsteinn.runarsson@netapp.com>
2020-02-09 00:50:20 +0000

    Using prometheus as Middleware

--- main.go
+++ main.go
@@ -3,23 +3,25 @@ package main
 import (
 	"fmt"
 	"log"
+	"strconv"
+	"time"
 	"net/http"
 
 	"github.com/gorilla/mux"
 
 	"github.com/prometheus/client_golang/prometheus"
-    //"github.com/prometheus/client_golang/prometheus/promauto"
+    "github.com/prometheus/client_golang/prometheus/promauto"
     "github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
 
 var (
-	inFlightGauge = prometheus.NewGauge(prometheus.GaugeOpts{
+	inFlightGauge = promauto.NewGauge(prometheus.GaugeOpts{
 		Name: "hello_requests_in_flight",
 		Help: "A gauge of requests currently being served by the wrapped handler.",
 	})
 
-	counter = prometheus.NewCounterVec(
+	counter = promauto.NewCounterVec(
 		prometheus.CounterOpts{
 			Name: "hello_requests_total",
 			Help: "A counter for requests to the wrapped handler.",
@@ -27,7 +29,7 @@ var (
 		[]string{"code", "method"},
 	)
 
-	requestDuration = prometheus.NewHistogramVec(
+	requestDuration = promauto.NewHistogramVec(
 		prometheus.HistogramOpts{
         	Name:    "hello_request_duration_seconds",
         	Help:    "Time (in seconds) spent serving HTTP requests.",
@@ -37,7 +39,7 @@ var (
 		[]string{"code", "method"},
 	)
 
-	responseSize = prometheus.NewHistogramVec(
+	responseSize = promauto.NewHistogramVec(
 		prometheus.HistogramOpts{
 			Name:    "hello_response_size_bytes",
 			Help:    "A histogram of response sizes for requests.",
@@ -49,29 +51,62 @@ var (
 
 	helloChain = promhttp.InstrumentHandlerInFlight(inFlightGauge,
 		promhttp.InstrumentHandlerDuration(requestDuration,
-		promhttp.InstrumentHandlerCounter(counter,
-			promhttp.InstrumentHandlerResponseSize(responseSize, SayHello()),
+			promhttp.InstrumentHandlerCounter(counter,
+				promhttp.InstrumentHandlerResponseSize(responseSize, SayHello()),
 			),
 		),
 	)
 )
 
-func init() {
-	// Metrics have to be registered to be exposed:
-	prometheus.MustRegister(
-		inFlightGauge,
-		counter,
-		requestDuration,
-		responseSize,
-	)
+//func init() {
+//	// Metrics have to be registered to be exposed:
+//	prometheus.MustRegister(
+//		inFlightGauge,
+//		counter,
+//		requestDuration,
+//		responseSize,
+//	)
+//}
+
+func MonitoringMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// Do stuff here
+		start := time.Now()
+		log.Println("I'm in your middleware")
+
+		// AHA!
+		defer func() {
+			duration := time.Since(start)
+			requestDuration.With(prometheus.Labels{"code": strconv.Itoa(http.StatusOK), "method": r.Method}).Observe(duration.Seconds())
+			log.Printf("The requuest took %f seconds tos serve", duration.Seconds())
+		}()
+
+		//responseSize.httpResponseSizeHistogram.Observe(float64(sizeBytes))
+
+		//inFlightGauge.httpRequestsInflight.Add(float64(quantity))
+
+		//func (r recorder) ObserveHTTPResponseSize(_ context.Context, p metrics.HTTPReqProperties, sizeBytes int64) {
+		//	r.httpResponseSizeHistogram.WithLabelValues(p.Service, p.ID, p.Method, p.Code).Observe(float64(sizeBytes))
+		//}
+		//
+		//func (r recorder) AddInflightRequests(_ context.Context, p metrics.HTTPProperties, quantity int) {
+		//	r.httpRequestsInflight.WithLabelValues(p.Service, p.ID).Add(float64(quantity))
+
+		// Call the next handler, which can be another middleware in the chain, or the final handler.
+		next.ServeHTTP(w, r)
+	})
 }
 
 func main() {
 	log.Println("Starting server, listening on port 8080")
 	router := mux.NewRouter()
-	router.Handle("/hello", helloChain)
-	router.Handle("/hello/{name}", helloChain)
+
+	router.Handle("/hello", SayHello())
+	router.Handle("/hello/{name}", SayHello())
 	router.Handle("/metrics", promhttp.Handler())
+
+	router.Use(MonitoringMiddleware)
+
 	log.Fatal(http.ListenAndServe(":8080", router))
 }
 
@@ -84,7 +119,7 @@ func SayHello() http.HandlerFunc {
 			name := vars["name"]
 
 			if name == "" {
-			    log.Println(r.URL.Path + " - 200 - Saying hello")
+				log.Println(r.URL.Path + " - 200 - Saying hello")
 			} else {
 				log.Println(r.URL.Path + " - 200 - Saying hello to " + name)
 			}
